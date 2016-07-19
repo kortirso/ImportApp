69bc341150313c18c64ff39355458fba16c0280a
kortirso <kortirso@gmail.com>
2016-07-19 17:50:23 +0300

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ImportApp
             g.test_framework :rspec, fixtures: true, views: false, view_specs: false, helper_specs: false,
                 routing_specs: false, controller_specs: true, request_specs: false
             g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        end
         config.active_record.raise_in_transactional_callbacks = true
     end
 end
